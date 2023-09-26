3cb36001b1c56194ab70fe03466c0c8d49f03970
sauravp <sauravpandey0022@gmail.com>
2023-09-26 16:29:05 +0530

    added builder function

--- src/main/java/com/example/demo/entity/JwtResponse.java
+++ src/main/java/com/example/demo/entity/JwtResponse.java
@@ -16,6 +16,47 @@ import lombok.ToString;
 public class JwtResponse {
 	private String jwtToken;
 	private String username;
-	
+	 public String getJwtToken() {
+	        return jwtToken;
+	    }
+
+	    public void setJwtToken(String jwtToken) {
+	        this.jwtToken = jwtToken;
+	    }
+
+	    public String getUsername() {
+	        return username;
+	    }
+
+	    public void setUsername(String username) {
+	        this.username = username;
+	    }
+	 public static JwtResponseBuilder builder() {
+	        return new JwtResponseBuilder();
+	    }
+
+	    // Inner builder class
+	    public static class JwtResponseBuilder {
+	        private String jwtToken;
+	        private String username;
+
+	        public JwtResponseBuilder jwtToken(String jwtToken) {
+	            this.jwtToken = jwtToken;
+	            return this;
+	        }
+
+	        public JwtResponseBuilder username(String username) {
+	            this.username = username;
+	            return this;
+	        }
+
+	        public JwtResponse build() {
+	            JwtResponse response = new JwtResponse();
+	            response.setJwtToken(this.jwtToken);
+	            response.setUsername(this.username);
+	            return response;
+	        }
+	    }
 
 }
+
